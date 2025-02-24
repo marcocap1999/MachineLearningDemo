939f9f9b1d5f777126a0b897fb09dbb60b4764c3
LucaGemma <42171162+LucaGemma@users.noreply.github.com>
2025-02-24 09:53:34 +0100

    Create regression.py
    Added regression.py with the import section.

---
+++ regression.py
@@ -0,0 +1,14 @@
+# --- IMPORT SECTION ---
+import pandas as pd # for DataFrames
+import numpy as np # for numpy array operations
+import matplotlib.pyplot as plt # for visualization
+# importing all the needed functions from scikit-learn
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler
+from sklearn.linear_model import LinearRegression, Ridge
+from sklearn.metrics import mean_squared_error
+# --- END OF IMPORT SECTION ---
+
+# --- MAIN CODE ---
+
+# --- END OF MAIN CODE ---
